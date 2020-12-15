01bf19434fda7d6f459063b0c533815f778ae86a
davidx315 <david315cg@hotmail.com>
2020-12-15 18:08:34 -0500

    Se crea el master del proyecto final

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <ImageText/>
+    <CarouselSection/>
+    <ServicesSection/>
+    <!--News-->
+
+        <div class="container-fluid"> 
+              <div class="d-flex justify-content-center align-items-center" >
+                <h2 class="tituloNoticias">Seccion de Noticias</h2>
+              </div>  
+                     
+              <NewsSection></NewsSection>         
+        </div>
+        
+
+    <!--Team-->
+    <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+          <div class="col mt-5" v-for="(miembro, index) of Team" :key="index">
+            <TeamCard :member="miembro"></TeamCard>
+          </div>
+            
+        </div>
+    </div>
+    <FooterSection/>
+  </div>
+</template>
+
+<script>
+
+import ImageText from './components/ImageText.vue'
+import CarouselSection from './components/CarouselSection.vue'
+import ServicesSection from './components/ServicesSection.vue'
+import FooterSection from './components/FooterSection.vue'
+import TeamCard from './components/TeamCard.vue'
+import NewsSection from './components/NewsSection.vue'
+
+export default {
+  name: 'App',
+  components:{
+    ImageText,
+    CarouselSection,
+    ServicesSection,
+    FooterSection,
+    TeamCard,
+    NewsSection
+  },
+  data(){
+    return{
+      // aqui va el listado del equipo en forma de JSON
+    }
+  }
+}
+</script>
+
+
+
---
+++ src/components/NewsSection.vue
@@ -0,0 +1,50 @@
+<template>          
+            <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+                
+                <div class="col-lg-6 col-xs-12" v-for="(pelicula, index) of peliculas" :key = index>
+                    <div class="d-flex justify-content-center align-items-center">
+                        
+                        <div class="p-3">
+                            <img class="imagenNews" :src="pelicula.image" alt="" width="300em" height="300em" style="border-radius: 10px">
+                        </div>
+        
+                        <div class="p-2">
+                            <h4>{{pelicula.title}}</h4>
+                            <h6>{{pelicula.description}}</h6>
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut ratione harum voluptates nulla laudantium! Aut, 
+                                possimus. Expedita unde maiores reprehenderit, quibusdam blanditiis beatae doloremque? Corporis laudantium veritatis omnis accusantium error.
+                            </p>
+                        </div>
+        
+                    </div>
+                    <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+                        <button  type="button" @click="pelicula.url" target="_blank" class="btn btn-outline-info"> Info</button>
+                    </div>
+                </div>
+           </div>
+</template>
+
+
+<script>
+import axios from "axios"
+
+    export default {
+        name: "NewsSection",
+        data(){
+            return{
+                peliculas: null
+            }
+            
+        },
+        created(){
+            axios
+                .get('https://gnews.io/api/v4/top-headlines?&token=dbb79ab882316df3fb72b08552a98dc8&max=10')
+                .then(response => { this.peliculas = response.data.articles.slice(0,4) })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
